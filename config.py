564fcbafdd6bef6e4180db0acd265cea0dee1bd2
135246508623 <339629740@qq.com>
2026-02-23 10:40:02 +0800

    Add SENTRY_BASE and BASE_TELEMETRY configuration

--- config.py
+++ config.py
@@ -1,1 +1,17 @@
+SENTRY_BASE = "https://sentry.platorelay.com/.gs/pow/captcha"
 
+BASE_TELEMETRY = {
+    "dwellMs": 446629,
+    "moves": 592,
+    "velocityVar": 17.2058786473109,
+    "velocityMedian": 1.455788671386738,
+    "velocityAvg": 3.2309785421350123,
+    "velocityMin": 0.0005871534893303571,
+    "velocityMax": 18.108148421848494,
+    "velocityP25": 0.42923229467905805,
+    "velocityP75": 3.793246599138705,
+    "directionChanges": 31,
+    "keypresses": 0,
+    "speedSamples": 592,
+    "moveDensity": 754.4408783783783
+}
